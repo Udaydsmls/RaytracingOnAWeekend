f014602a9d9c6dc5ea0802aa9c5e370550bf2ea6
Uday S <udayds12mld@gmail.com>
2024-11-11 00:05:08 -0500

    Two new Libs

---
+++ color.h
@@ -0,0 +1,26 @@
+//
+// Created by Uday S on 11-11-2024.
+//
+
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+
+using color = vec3;
+
+void write_color(std::ostream &out, const vec3 &pixel_color) {
+    const auto r = pixel_color.x();
+    const auto g = pixel_color.y();
+    const auto b = pixel_color.z();
+
+    const int r_byte = static_cast<int>(255.99 * r);
+    const int g_byte = static_cast<int>(255.99 * g);
+    const int b_byte = static_cast<int>(255.99 * b);
+
+    out << r_byte << " " << g_byte << " " << b_byte;
+}
+
+
+#endif //COLOR_H

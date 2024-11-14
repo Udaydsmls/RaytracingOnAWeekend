466d996a83f85c7e6f6bc63dee04aa440628b7a1
Uday S <udayds12mld@gmail.com>
2024-11-14 03:30:10 -0500

    Update 1

--- main.cpp
+++ main.cpp
@@ -4,10 +4,29 @@
 #include "color.h"
 #include "ray.h"
 
-color ray_color(const ray& r) {
-    const vec3 unit_direction = unit_vector(r.direction());
-    const double a = 0.5*(unit_direction.y() + 1.0);
-    return (1.0 - a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+
+double hit_sphere(const point3 &center, double radius, const ray &r) {
+    vec3 oc = center - r.origin();
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
+    if (discriminant < 0)
+        return -1.0;
+
+    return (h - std::sqrt(discriminant)) / a;
+}
+
+color ray_color(const ray &r) {
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
@@ -19,7 +38,7 @@ int main() {
 
     auto focal_length = 1.0;
     auto viewport_height = 2.0;
-    auto viewport_width = viewport_height * ((double)image_width/image_height);
+    auto viewport_width = viewport_height * ((double) image_width / image_height);
     auto camera_center = vec3(0, 0, 0);
 
     auto viewport_u = vec3(viewport_width, 0, 0);
@@ -28,7 +47,7 @@ int main() {
     auto pixel_delta_u = viewport_u / image_width;
     auto pixel_delta_v = viewport_v / image_height;
 
-    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
     auto pixel00_cc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
